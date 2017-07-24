386feffd9c2f88401fce8c9fd0b74aec5ae0ae38
Pablo Saïd <azeupaul@gmail.com>
2017-07-24 21:56:16 +0100

    Ajout du formulaire d'ajout de contact en lecture seule

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var contacts = [
   {key: 3, name: "Joe Doe"},
 ]
 
-// Contact Item Component
+// ContactItem Component
 var ContactItem = React.createClass({
   propTypes: {
     name: React.PropTypes.string.isRequired,
@@ -21,14 +21,32 @@ var ContactItem = React.createClass({
   }
 })
 
+// ContactForm Component
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+  render: function(){
+    return React.createElement('form', {},
+      React.createElement('input', {placeholder: 'Name (required)', value: this.props.contact.name, type: 'text'}),
+      React.createElement('input', {placeholder: 'Email (required)', value: this.props.contact.email, type: 'email'}),
+      React.createElement('textarea', {placeholder: 'Description', value: this.props.contact.description}, ''),
+      React.createElement('button', {type: 'Submit'}, 'Save')
+    )
+  }
+})
+
 var contactItemList = []
 
 for(var i = 0; i < contacts.length; i++){
   contactItemList.push(React.createElement(ContactItem, contacts[i]))
 }
 
+var newContact = {name: "", email: "", description: ""}
+
 var rootElement = React.createElement('div', {},
   React.createElement('h1', {}, 'Contacts'),
+  React.createElement(ContactForm, {contact: newContact}),
   React.createElement('ul', {}, contactItemList)
 )
 ReactDOM.render(
